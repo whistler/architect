d884620a8335f22ee1e0acddeeaf7c27e91ecf08
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-10-10 22:43:00 -0700

    Use nothugly XSLT and output SVG format

--- architect.gemspec
+++ architect.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "ruby-graphviz"
+  spec.add_dependency "libxslt-ruby"
   spec.add_dependency "listen", "2.0"
 
   spec.add_development_dependency "bundler", "~> 1.3"
--- lib/architect/diagram.rb
+++ lib/architect/diagram.rb
@@ -7,16 +7,16 @@ module Architect
   class Diagram
     # Draw
     # [diagram] string containing the markup of the diagram
-    def draw(diagram, output = "class_diagram.png", ext = 'png')
+    def draw(diagram, output = "class_diagram.svg", ext = 'svg')
       parser = Parser.new
       elements = parser.parse(diagram)
       graph = GraphViz.new("ClassDiagram", type: "digraph")
-      graph.node["fillcolor"] = "gray95"
+      graph.node["fillcolor"] = "lightgrey"
       graph.node["style"] = "filled"
       elements.each do |element|
         element.graph(graph)
       end
-      graph.output(ext.to_sym => output)
+      graph.output(ext.to_sym => output, nothugly: true)
     end
     
   end
